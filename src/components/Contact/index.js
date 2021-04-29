4f21046b8beb5e28a20609a3b5d826ab0ff78b6e
albenchris <albenchris00@gmail.com>
2021-04-29 16:30:37 -0500

    basic contact section

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,6 +7,38 @@ const Contact = () => {
             <h2 id="contact">
                 Contact Me
             </h2>
+            <form
+                id="contact-form"
+                // onSubmit={handleSubmit}
+            >
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input
+                        type="text"
+                        name="name"
+                        // defaultValue={name}
+                        // onBlur={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="text"
+                        name="email"
+                        // defaultValue={email}
+                        // onBlur={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea
+                        name="message"
+                        rows="5"
+                        // defaultValue={message}
+                        // onBlur={handleChange}
+                    />
+                </div>
+            </form>
         </section>
     );
 }
