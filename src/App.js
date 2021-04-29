0357e0eebe42c327c1de6ac6ad02b0e59a903bac
albenchris <albenchris00@gmail.com>
2021-04-29 14:09:42 -0500

    starting lines for each component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Project from './components/Project';
 
 function App() {
   return (
     <div>
       Hello
+      <Header />
+      <About />
+      <Project />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/About/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = () => {
+
+    return (
+        <section>About</section>
+    );
+}
+
+export default About;
