349253d435e85ce3e3cb8426955b3bf9cdc81558
albenchris <albenchris00@gmail.com>
2021-04-30 09:30:59 -0500

    added links to github, linkedin, stack overflow to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,31 @@ import React from 'react';
 const Footer = () => {
 
     return (
-        <footer>Footer</footer>
+        <footer>
+            <a
+                href="https://github.com/albenchris"
+                target="_blank"
+                rel="norefferer"
+            >
+                GitHub
+            </a>
+            {" "}
+            <a
+                href="https://www.linkedin.com/in/alexander-christopherson-2b32085a/"
+                target="_blank"
+                rel="norefferer"
+            >
+                LinkedIn
+            </a>
+            {" "}
+            <a
+                href="https://stackoverflow.com/users/14658140/alexander-christopherson"
+                target="_blank"
+                rel="norefferer"
+            >
+                Stack Overflow
+            </a>
+        </footer>
     );
 }
 
