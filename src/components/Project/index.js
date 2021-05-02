f5bc03e3b12d1d440000532bbd8cf6241473ff9a
albenchris <albenchris00@gmail.com>
2021-05-02 14:48:09 -0500

    more Portfolio and Project styles

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,24 +6,29 @@ const Project = (props) => {
     const { name, image, repositoryLink, deployedLink } = props;
 
     return (
-        <div className="card">
-            <div className="image-container">
-                <img alt={name} src={image} />
-            </div>
-            <div className="content">
-                <h3 className="project-name">{name}</h3>
-                <div className="link-container">
-                    <div className="project-link">
-                        <a href={deployedLink} target="_blank" rel="noreferrer">
-                            deployed
-                        </a>
-                    </div>
-                    <div className="project-link">
-                        <a href={repositoryLink} target="_blank" rel="noreferrer">
-                            repository
-                        </a>
+        <div className="flip-card">
+            <div className="flip-card-inner">
+
+                <div className="image-container flip-card-front">
+                    <img alt={name} src={image} />
+                </div>
+
+                <div className="content flip-card-back">
+                    <h3 className="project-name">{name}</h3>
+                    <div className="link-container">
+                        <div className="project-link">
+                            <a href={deployedLink} target="_blank" rel="noreferrer">
+                                deployed
+                            </a>
+                        </div>
+                        <div className="project-link">
+                            <a href={repositoryLink} target="_blank" rel="noreferrer">
+                                repository
+                            </a>
+                        </div>
                     </div>
                 </div>
+
             </div>
         </div>
     );
